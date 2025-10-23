fe7ca6c78f642279484a967e34220c47ba64720c
Isabella BABATUNDE <bellabaly06@gmail.com>
2025-10-23 10:47:58 +0000

    Correction des bug

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Button.css"; 
 
 type ButtonProps = {
-  label: string;
+  label?: string;
   onClick?: () => void;
   type?: "button" | "submit" | "reset";
   disabled?: boolean;
